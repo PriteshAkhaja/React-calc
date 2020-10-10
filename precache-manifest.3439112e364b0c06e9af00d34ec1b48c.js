7e9eb1f582b9b5bb14d2598dfe56c2564f50382b
PriteshAkhaja <priteshakhaja1234@gmail.com>
2020-10-10 09:50:16 +0530

    Updates

---
+++ precache-manifest.3439112e364b0c06e9af00d34ec1b48c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a443ccbe03f07a757108be23cba03d",
+    "url": "/React-calc/index.html"
+  },
+  {
+    "revision": "76173660113202d758d1",
+    "url": "/React-calc/static/css/main.fc8f4aa2.chunk.css"
+  },
+  {
+    "revision": "f8fd0d447a0caa3e6b65",
+    "url": "/React-calc/static/js/2.946236d5.chunk.js"
+  },
+  {
+    "revision": "76173660113202d758d1",
+    "url": "/React-calc/static/js/main.1271cd2c.chunk.js"
+  },
+  {
+    "revision": "3ad79d55ee60341ceac4",
+    "url": "/React-calc/static/js/runtime~main.b325aa39.js"
+  }
+]);
